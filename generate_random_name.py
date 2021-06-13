fd134b28bf311f8188e9ba9de8813d88c40d62a5
Aurora <qm2737@gmail.com>
2021-06-13 16:00:43 +0800

    修改生效时间最晚为当前时间

--- generate_random_name.py
+++ generate_random_name.py
@@ -4,11 +4,11 @@
 # @Personal website: ysbzc.com
 # @CreateTime: 2021/6/11 22:47
 
-import random,string
+import random, string
 
 seeds = string.digits
 ran_digit = random.sample(seeds, k=3)
 app_suffix = "".join(ran_digit)
 
-vars.put("applicationCaption", "application" + app_suffix);
-vars.put("applicationName", "application" + app_suffix);
+vars.put("applicationCaption", "application" + app_suffix)
+vars.put("applicationName", "application" + app_suffix)
