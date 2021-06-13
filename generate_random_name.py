1060fbc4c7bc693fd92ad64687c52143e2bf4262
Aurora <qm2737@gmail.com>
2021-06-13 15:56:00 +0800

    修改生效时间最晚为当前时间

---
+++ generate_random_name.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+# @ModuleName: MeterSphere
+# @Author: Moon-du
+# @Personal website: ysbzc.com
+# @CreateTime: 2021/6/11 22:47
+
+import random,string
+
+seeds = string.digits
+ran_digit = random.sample(seeds, k=3)
+app_suffix = "".join(ran_digit)
+
+vars.put("applicationCaption", "application" + app_suffix);
+vars.put("applicationName", "application" + app_suffix);
