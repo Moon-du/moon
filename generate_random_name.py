04d2e920266e6259df7d7673d694be6d4483e793
Aurora <qm2737@gmail.com>
2021-06-13 16:17:50 +0800

    修改生效时间最晚为当前时间

--- generate_random_name.py
+++ generate_random_name.py
@@ -8,7 +8,9 @@ import random, string
 
 seeds = string.digits
 ran_digit = random.sample(seeds, k=3)
-app_suffix = "".join(ran_digit)
 
-vars.put("applicationCaption", "application" + app_suffix)
-vars.put("applicationName", "application" + app_suffix)
+prefix = "application"
+suffix = "".join(ran_digit)
+
+vars.put("applicationCaption", prefix + suffix)
+vars.put("applicationName", prefix + suffix)
